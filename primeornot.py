cd13b8abe2bbbc6c4f7553f363ba0d5f2736545e
Shaheer25 <98147716+Shaheer25@users.noreply.github.com>
2022-06-12 11:01:16 +0530

    Create primeornot.py

---
+++ primeornot.py
@@ -0,0 +1,11 @@
+x=19991 #enter any Number to check whether prime or not
+if(x==0):
+    print("%d is not a prime"%x)
+elif(x%2==0):
+    print("%d is not a prime"%x)
+elif(x%3==0):
+    print("%d is not a prime"%x)
+elif(x%5==0):
+    print("%d is not a prime"%x)
+else:
+    print("%d is a prime Number"%x)
